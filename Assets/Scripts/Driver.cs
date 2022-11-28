9c38c684e673539e2b3a9e18358b49462d01e5dd
William Barnes <jakebarnes1989@gmail.com>
2022-11-27 21:18:26 -0800

    Update Driver.cs
    Implemented boost and slow functionality with coroutine timer.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Initialize the variables
+    [SerializeField] float defaultSpeed = 10.0f;
     [SerializeField] float moveSpeed = 10.0f;
     [SerializeField] float steerSpeed = 100.0f;
+    [SerializeField] float slowSpeed = 5.0f;
+    [SerializeField] float slowDuration = 2.0f;
+    [SerializeField] float boostSpeed = 15.0f;
+    [SerializeField] float boostDuration = 2.0f;
 
     // Update is called once per frame
     void Update()
@@ -25,4 +25,39 @@ public class Driver : MonoBehaviour
         transform.Translate(0, moveAmount, 0);
         transform.Rotate(0, 0, -steerAmount);
     }
+
+    // OnTriggerEnter2D that sets moveSpeed to boostSpeed, and then after boostDuration seconds, sets moveSpeed back to defaultSpeed
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+            Debug.Log("Boost!");
+            StartCoroutine(BoostTimer());
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        // If other gameObject is untagged, set moveSpeed to slowSpeed
+        if (other.gameObject.tag == "Slow")
+        {
+            moveSpeed = slowSpeed;
+            Debug.Log("Crash!");
+            StartCoroutine(SlowTimer());
+        }
+    }
+
+    // Coroutine that sets moveSpeed back to defaultSpeed after boostDuration seconds
+    IEnumerator BoostTimer()
+    {
+        yield return new WaitForSeconds(boostDuration);
+        moveSpeed = defaultSpeed;
+    }
+
+    // Coroutine that sets moveSpeed back to defaultSpeed after slowDuration seconds
+    IEnumerator SlowTimer()
+    {
+        yield return new WaitForSeconds(slowDuration);
+        moveSpeed = defaultSpeed;
+    }
+
 }
