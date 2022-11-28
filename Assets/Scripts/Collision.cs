7f9ed2e4b2df7e6f864f3e2e6524fcc3b67f5f81
Jessica Alder <jakebarnes1989@gmail.com>
2022-11-27 16:33:50 -0800

    Update Collision.cs
    - Added collision logic for picking up package and destroying object.
    - Updated debug log for colliding with hard object.

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    bool hasPackage = false;
+    [SerializeField] float destroyDelay = 0.5f;
+    [SerializeField] Color32 colorHasPackage = new Color32(255, 255, 255, 255);
+
     void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("Collision with " + other.gameObject.name);
+        Debug.Log("Ouch!");
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("Trigger with " + other.gameObject.name);
+
+        if (other.tag == "Package" && hasPackage)
+        {
+            Destroy(other.gameObject, destroyDelay);
+            hasPackage = true;
+            Debug.Log("Package picked up!");
+        }
+
+        // If other gameObject tag is Customer, print "Delivery Complete"
+        if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Delivery Complete");
+            hasPackage = false;
+        }
+        else if (other.tag == "Customer" && !hasPackage)
+        {
+            Debug.Log("You don't have the package!");
+        }
     }
 }
